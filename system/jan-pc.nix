5b1c95ea6f990271c67a23c5bf4657f76dc738fa
Jan Kot <janymalino@gmail.com>
2026-04-19 20:19:43 +0200

    jan-pc: remove ddcci configuration; it has to be reworked

--- system/jan-pc.nix
+++ system/jan-pc.nix
@@ -77,37 +77,4 @@
       ];
     };
   };
-
-  # ddcci
-  hardware.i2c.enable = true;
-  systemd.services."ddcci@" = {
-    description = "DDC/CI device auto-attachment for NVIDIA";
-    scriptArgs = "%i";
-    script = ''
-      		echo Trying to attach ddcci to $1
-      		id=$(echo $1 | cut -d "-" -f 2)
-      		if ${pkgs.ddcutil}/bin/ddcutil getvcp 10 -b $id --discard-cache; then
-      			echo "ddcci 0x37" > /sys/bus/i2c/devices/$1/new_device
-      			echo "ddcci attached to $1"
-      		fi
-      	'';
-    serviceConfig.Type = "oneshot";
-  };
-
-  services.udev.extraRules = ''
-    SUBSYSTEM=="i2c-dev", ACTION=="add",\
-      ATTR{name}=="NVIDIA i2c adapter*",\
-      TAG+="ddcci",\
-      TAG+="systemd",\
-      ENV{SYSTEMD_WANTS}+="ddcci@$kernel.service"
-  '';
-
-  boot.extraModulePackages = with config.boot.kernelPackages; [ ddcci-driver ];
-  boot.kernelModules = [
-    "ddcci_backlight"
-    "kvm-intel"
-  ];
-  environment.systemPackages = with pkgs; [
-    ddcutil
-  ];
 }
